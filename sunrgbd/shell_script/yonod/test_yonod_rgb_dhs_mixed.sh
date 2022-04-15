5ff04895d19ccda7080fd217c65f72161858eaea
jimmy shen <jim.morris.shen@gmail.com>
2022-04-14 23:05:28 -0400

    Add rgb dhs mixed on rgb result

--- sunrgbd/shell_script/yonod/test_yonod_rgb_dhs_mixed.sh
+++ sunrgbd/shell_script/yonod/test_yonod_rgb_dhs_mixed.sh
@@ -1,8 +1,8 @@
 # Test swin with rgb_dhs_mixed checkpoints on RGB images                    
-cfg=configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb_dhs_mixed_test_rgb.py
+cfg=configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_coco_RGB_load_from.py
 res_savepath=./test_logs_unshow/rgb_dhs_mixed_on_rgb/
 mkdir -p ${res_savepath}
-for (( i=100; i>=0; i-- ))
+for (( i=100; i>0; i-- ))
 do
 echo "epoch ${i}"
 checkpoint=/data/sophia/a/Xiaoke.Shen54/repos/SwinTransFusion/work_dirs/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb_dhs_mixed/epoch_${i}.pth
