64ffa883ccdac67c4d689f7c0e7b244a74c6a635
jimmy shen <jim.morris.shen@gmail.com>
2022-04-25 22:42:48 -0400

    Train rgbdhs mixed with stochastic flood fill aug images (#23)

---
+++ sunrgbd/shell_script/yonod/train_swin_transform_rgb_dhs_mixed_with_sff.sh
@@ -0,0 +1,5 @@
+
+# Train both rgb and dhs images at the same time with pretrained weights from COCO dataset based on RGB images
+cfg=configs/swin/mask_rcnn_swin-t-p4-w7_fpn_ms-crop-3x_sunrgbd_rgb_dhs_mixed_with_sff.py 
+echo "train rgb and dhs"
+python tools/train.py  ${cfg} > train_swin_rgbdhs_with_sff_200epochs.log 
