a818242ce3b5795a8b54c68631f2eef3695434cc
jimmy shen <jim.morris.shen@gmail.com>
2022-06-21 18:05:39 -0400

    Add missed logs

---
+++ sunrgbd/shell_script/yonod/test_swin_small_transform_rgb_dhs_mixed.sh
@@ -0,0 +1,11 @@
+# Test swin with rgb_dhs_mixed with sff checkpoints on RGB images                    
+cfg=./work_dirs/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed_test_on_rgb.py
+res_savepath=./test_logs_unshow/rgb_dhs_mixed_swin_s_on_rgb/
+mkdir -p ${res_savepath}
+for (( i=100; i>0; i-- ))
+do
+echo "epoch ${i}"
+checkpoint=/data/sophia/a/Xiaoke.Shen54/repos/SwinTransFusion/work_dirs/mask_rcnn_swin-s-p4-w7_fpn_fp16_ms-crop-3x_sunrgbd_rgbdhs_mixed/epoch_${i}.pth
+python tools/test.py  ${cfg} ${checkpoint} --eval=bbox  > ${res_savepath}test_swin_rgb_dhs_mixed_with_chess1_yonod_on_rgb_epoch_${i}.log
+done
+
